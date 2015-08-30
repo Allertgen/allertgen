3407eac760ab2d6c121dc7445baf3c7856e35013
Kevin Chiang <kevinwchiang@gmail.com>
2015-08-29 18:20:39 -0700

    Yelp API set up

--- client/landingPage/landingPage.js
+++ client/landingPage/landingPage.js
@@ -1,13 +1,25 @@
 angular.module('app.landingPage',[])
 .controller('landingPageController', 
   ['$scope', '$http', '$location', function ($scope, $http, $location) {
-  $scope.login = function () {
-    
-  };
 
   $scope.search = function(restaurant, location){
-    //perform API calls to get info that we can render on searchPage
-    $location.path('/searchPage');
+    console.log(restaurant, location);
+
+    //perform API call to get info that we can render on searchPage
+    $http.get('/yelp', {
+      params: {
+        restaurant: restaurant,
+        location: location
+      }
+    }).then(function(res){
+      console.log('THIS IS THE RESPONSE: ', res.data);
+      $scope.searchResults = res.data;
+      console.log('SCOPE.SEARCHRESULTS', $scope.searchResults);
+    })
+    // .then(function(){
+    //   //go to search page
+    //   $location.path('/searchPage');
+    // })
   };
 
 }]);
--- server/server.js
+++ server/server.js
@@ -2,10 +2,29 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-//yelp API call dependencies
 
 //serve up static index.html file by telling express to look in the client folder
 app.use('/', express.static("./client"));
+
+//yelp client API configuration
+var yelp = require("yelp").createClient({
+  consumer_key: "Rrq-4MDBHD_fizozFeSbJQ",
+  consumer_secret: "wk-eJnt7_2pLHJ8Jrty4DN3T7TY",
+  token: "PwoAmHcHLys2zckRR7bkbRq7osaNycNm",
+  token_secret: "j3_6MRTFrVLCaVr-pomty9J3pX4"
+});
+
+module.exports = 'yelp';
+//yelp API call using request sent from front end
+app.get('/yelp', function(req, res){
+  yelp.search({term: req.query.restaurant, location: req.query.location}, function(error, data) {
+    if (error){
+      console.error(error);
+    } else {
+      res.send(data);
+    }
+  });
+});
 
 
 
